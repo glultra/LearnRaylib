bda3c1e7269965b71218dd27219db6e29293499c
glULTRA <7amakurd2002@gmail.com>
2022-06-20 18:05:04 +0300

    Main File

--- main.cpp
+++ main.cpp
@@ -8,13 +8,6 @@ int main(){
     // Initialize window.
     InitWindow(SCREEN_WIDHT, SCREEN_HEIGHT, SCREEN_TITLE);
 
-    // Rectangle attributes
-    Vector2 rec_pos = Vector2{300, 150};
-    Vector2 rec_size = Vector2{100, 100};
-    Vector2 rec_origin = Vector2{50, 50};
-
-    Rectangle rect = Rectangle{rec_pos.x, rec_pos.y, rec_size.x ,rec_size.y};
-
     // Render loop.
     while (!WindowShouldClose())
     {
@@ -25,17 +18,7 @@ int main(){
             // Clear Background
             ClearBackground(WHITE);
             // <--- DRAW --->
-            rect.y = 100;
-            DrawRectangleGradientEx(rect, ORANGE, PURPLE, GREEN, PINK);
-            DrawRectangleLines(rect.x, rect.y, rect.width, rect.height, BLACK);
-            DrawRectangleLinesEx(rect, 3, BLACK);
-            rect.y = 350;
-            DrawRectanglePro(rect, rec_origin, 45, ORANGE);
-
-            rect.y = 470;
-            DrawRectangleRounded(rect, 100, 100, RED);
-            DrawRectangleRoundedLines(rect, 100, 100, 5, BLACK);
-            
+            DrawRectangle(150, 50, 100, 100, ORANGE);
         EndDrawing();
     }
     
