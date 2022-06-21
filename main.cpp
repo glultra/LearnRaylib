7d754dbde87e3dce95fc2dfdf4d2fedf9e341ab0
glULTRA <7amakurd2002@gmail.com>
2022-06-21 19:18:55 +0300

    Lines and curves

--- main.cpp
+++ main.cpp
@@ -8,6 +8,10 @@ int main(){
     // Initialize window.
     InitWindow(SCREEN_WIDHT, SCREEN_HEIGHT, SCREEN_TITLE);
 
+    // Line properties.
+    Vector2 start_pos = Vector2{100, 100};
+    Vector2 end_pos = Vector2{200, 100};
+
     // Render loop.
     while (!WindowShouldClose())
     {
@@ -18,7 +22,13 @@ int main(){
             // Clear Background
             ClearBackground(WHITE);
             // <--- DRAW --->
-            DrawRectangle(150, 50, 100, 100, ORANGE);
+            // DrawLine(100, 100, 200, 200, RED);
+            // DrawLineV(Vector2{100, 100}, Vector2{200, 200}, BLACK);
+            // DrawLineEx(start_pos, end_pos, 10, BLACK);
+            // DrawLineBezier(start_pos, end_pos, 5, GREEN);
+            // DrawLineBezier(Vector2{200, 200}, Vector2{300, 100}, 5, PURPLE);
+            // DrawLineBezierCubic(start_pos, end_pos, GetMousePosition(), GetMousePosition(), 3, ORANGE);
+            DrawLineBezierQuad(start_pos, end_pos, GetMousePosition(), 3, GREEN);
         EndDrawing();
     }
     
