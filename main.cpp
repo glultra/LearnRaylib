c15c5d4a9e8435a06703ad9be1e6c127b585570e
glULTRA <7amakurd2002@gmail.com>
2022-06-22 23:35:14 +0300

    Keyboard input

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <raylib.h>
+#include <iostream>
 
 #define SCREEN_WIDHT    600
 #define SCREEN_HEIGHT   600
@@ -8,18 +9,29 @@ int main(){
     // Initialize window.
     InitWindow(SCREEN_WIDHT, SCREEN_HEIGHT, SCREEN_TITLE);
 
+    // Rectangle Object.
+    Vector2 rect_pos = Vector2{250, 250};
+
     // Render loop.
     while (!WindowShouldClose())
     {
         // <----- UPDATE ----->
+        if(IsKeyDown(KEY_UP))
+            rect_pos.y -= 1.0f;
+        else if(IsKeyDown(KEY_DOWN))
+            rect_pos.y += 1.0f;
+
+        if(IsKeyDown(KEY_RIGHT))
+            rect_pos.x += 1.0f;
+        else if(IsKeyDown(KEY_LEFT))
+            rect_pos.x -= 1.0f;
 
         // <----- RENDER ----->
         BeginDrawing();
             // Clear Background
             ClearBackground(WHITE);
             // <--- DRAW --->
-            DrawRectangle(150, 50, 100, 100, ORANGE);
-
+            DrawRectangleV(rect_pos, Vector2{100, 100}, RED);
         EndDrawing();
     }
 
