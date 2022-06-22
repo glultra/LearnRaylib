5ed2354b68fd8a75f81805bb94fd2a260eaf2ac0
glULTRA <7amakurd2002@gmail.com>
2022-06-22 19:50:45 +0300

    Added Font

--- main.cpp
+++ main.cpp
@@ -8,10 +8,6 @@ int main(){
     // Initialize window.
     InitWindow(SCREEN_WIDHT, SCREEN_HEIGHT, SCREEN_TITLE);
 
-    // Font object.
-    Font font = LoadFont("./res/fonts/Moby.ttf");
-    Font font1 = LoadFont("./res/fonts/setback.png");
-
     // Render loop.
     while (!WindowShouldClose())
     {
@@ -22,9 +18,8 @@ int main(){
             // Clear Background
             ClearBackground(WHITE);
             // <--- DRAW --->
-            DrawText("Hello World", 300, 300, 25, RED);
-            DrawTextEx(font, "Hello world", Vector2{100, 50}, 45, 2, BLUE);
-            DrawTextPro(font1, "Hello world", Vector2{70, 400}, Vector2{50, 50}, 45, 30, 5, PURPLE);
+            DrawRectangle(150, 50, 100, 100, ORANGE);
+
         EndDrawing();
     }
 
