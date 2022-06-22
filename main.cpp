24658726c73c6dba95ac168c11f6f0bd287fefec
glULTRA <7amakurd2002@gmail.com>
2022-06-22 19:12:33 +0300

    Texture part 2

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <raylib.h>
+#include <math.h>
+#include <iostream>
 
 #define SCREEN_WIDHT    600
 #define SCREEN_HEIGHT   600
@@ -10,6 +12,8 @@ int main(){
 
     // Texture 2D.
     Texture2D pink_panther = LoadTexture("./res/images/pink-panther.png");
+    pink_panther.height = 300;
+    pink_panther.width = 150;
 
     // Render loop.
     while (!WindowShouldClose())
@@ -17,6 +21,10 @@ int main(){
         // <----- UPDATE ----->
         // Toggle button (NO GUI yet).
         static bool isTurnedOn = false;
+        float time = GetTime();
+        float xValue = std::sin(time); // -1.0 to +1.0
+        std::cout << xValue << std::endl;
+
         if(IsKeyPressed(KEY_SPACE)){
             // if true return false.
             // else if false return true.
@@ -28,7 +36,11 @@ int main(){
             // Clear Background
             ClearBackground(WHITE);
             // <--- DRAW --->
-            DrawTexture(pink_panther, 10, 10, isTurnedOn ? WHITE : BROWN);
+            // DrawTexture(pink_panther, 10, 10, isTurnedOn ? WHITE : BROWN);
+            // DrawTextureEx(pink_panther, Vector2{10, 10}, 45, 1.0, WHITE);
+            // DrawTextureRec(pink_panther, Rectangle{40, 0, 100, 100},Vector2{10, 10}, WHITE );
+            DrawTextureTiled(pink_panther, Rectangle{40, 0, 100, 100}, Rectangle{0, 0, 600, 600}, Vector2{10, 10}, 360 * xValue, 1, WHITE);
+            // DrawTextureQuad(pink_panther, Vector2{5, 5}, Vector2{5, 5}, Rectangle{0, 0, 600, 600}, WHITE);
         EndDrawing();
     }
     
