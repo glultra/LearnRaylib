386549a5a258b6e803162bd2ea10debe64454545
glULTRA <7amakurd2002@gmail.com>
2022-06-22 23:51:02 +0300

    Added mouse and cursor input example

--- main.cpp
+++ main.cpp
@@ -8,17 +8,28 @@ int main(){
     // Initialize window.
     InitWindow(SCREEN_WIDHT, SCREEN_HEIGHT, SCREEN_TITLE);
 
+    // Rectangle position.
+    Vector2 rect_pos{100.0f, 100.0f};
+
     // Render loop.
     while (!WindowShouldClose())
     {
         // <----- UPDATE ----->
+        static Color color = ORANGE;
+        Vector2 mouse_pos = GetMousePosition();
+        if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+            color = GREEN;
+        else if(IsMouseButtonPressed(MOUSE_BUTTON_RIGHT))
+            color = ORANGE;
 
         // <----- RENDER ----->
         BeginDrawing();
             // Clear Background
             ClearBackground(WHITE);
             // <--- DRAW --->
-            DrawRectangle(150, 50, 100, 100, ORANGE);
+            DrawLineBezier(Vector2{0, 0}, mouse_pos, 4, color);
+            DrawRectangleV(Vector2{mouse_pos.x - 50, mouse_pos.y - 50}, Vector2{100, 100}, color);
+            DrawCircleV(mouse_pos, 30, WHITE);
 
         EndDrawing();
     }
