254cb65213bb761a3eceeef0ccb571d8a29105cc
glULTRA <7amakurd2002@gmail.com>
2022-06-22 15:52:30 +0300

    Main old file

--- main.cpp
+++ main.cpp
@@ -8,27 +8,17 @@ int main(){
     // Initialize window.
     InitWindow(SCREEN_WIDHT, SCREEN_HEIGHT, SCREEN_TITLE);
 
-    // Texture 2D.
-    Texture2D pink_panther = LoadTexture("./res/images/pink-panther.png");
-
     // Render loop.
     while (!WindowShouldClose())
     {
         // <----- UPDATE ----->
-        // Toggle button (NO GUI yet).
-        static bool isTurnedOn = false;
-        if(IsKeyPressed(KEY_SPACE)){
-            // if true return false.
-            // else if false return true.
-            isTurnedOn = isTurnedOn ? false : true; // if is true returns first case else returns second case.
-        }
 
         // <----- RENDER ----->
         BeginDrawing();
             // Clear Background
             ClearBackground(WHITE);
             // <--- DRAW --->
-            DrawTexture(pink_panther, 10, 10, isTurnedOn ? WHITE : BROWN);
+            DrawRectangle(150, 50, 100, 100, ORANGE);
         EndDrawing();
     }
     
