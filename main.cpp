401c31131bef3ad1378166bb22cb70127a36d383
glULTRA <7amakurd2002@gmail.com>
2022-06-22 15:12:58 +0300

    Pixels

--- main.cpp
+++ main.cpp
@@ -1,25 +1,53 @@
 #include <raylib.h>
+#include <cmath>
+#include <time.h>
+#include <vector>
 
 #define SCREEN_WIDHT    600
 #define SCREEN_HEIGHT   600
 #define SCREEN_TITLE    "Raylib"
 
 int main(){
+    // Generate Random numbers.
+    srand(time(NULL));
+
     // Initialize window.
     InitWindow(SCREEN_WIDHT, SCREEN_HEIGHT, SCREEN_TITLE);
+
+    // Pixel vector.
+    std::vector<Vector2> pixels;
+
+    Color colors[] = {
+        RED,GREEN,BLUE,PURPLE,ORANGE,YELLOW,BLACK,BROWN
+    };
 
     // Render loop.
     while (!WindowShouldClose())
     {
         // <----- UPDATE ----->
 
+        // Generating random pixels;
+        for(int i = 0; i < SCREEN_WIDHT; i++){
+            for(int j = 0; j < SCREEN_HEIGHT; j++){
+                Vector2 pix;
+                pix.x = rand() % 600;
+                pix.y = rand() % 600;
+                pixels.push_back(pix);
+            }
+        }
+
         // <----- RENDER ----->
         BeginDrawing();
             // Clear Background
             ClearBackground(WHITE);
             // <--- DRAW --->
-            DrawRectangle(150, 50, 100, 100, ORANGE);
+            for(Vector2 pixel : pixels){
+                DrawPixelV(pixel, colors[rand() % 8]);
+            }
         EndDrawing();
+
+        // Clear vector.
+        pixels.clear();
     }
     
 }
