f373b7c0deeceb55bebef672d719934188f91cd9
glULTRA <7amakurd2002@gmail.com>
2022-06-23 18:00:49 +0300

    Main OLD File

--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
 #include <raylib.h>
-#include <string.h>
 #include <iostream>
 #include <cmath>
-#include "src/timer.h"
+#include <string.h>
 
 #define SCREEN_WIDHT    600
 #define SCREEN_HEIGHT   600
@@ -12,35 +11,22 @@ int main(){
     // Initialize window.
     InitWindow(SCREEN_WIDHT, SCREEN_HEIGHT, SCREEN_TITLE);
 
-    // Rectangle object.
-    Rectangle rect = Rectangle{250, 250, 100, 100};
 
-    // Timer object.
-    Timer timer;
-    
-    // Settarget fps.
-    SetTargetFPS(60); // 60 picture per second -> 1s = 60frame
+    // Target FPS.
+    SetTargetFPS(60);
 
     // Render loop.
     while (!WindowShouldClose())
     {
         // <----- UPDATE ----->
-        float time = GetTime();
-        timer.UpdateTime(time);
 
-        rect.x = 200 + (timer.xValue * 200 );
-        rect.y = 200 + (timer.yValue * 200 );
-        
         // <----- RENDER ----->
         BeginDrawing();
             // Clear Background
             ClearBackground(WHITE);
             // <--- DRAW --->
-            DrawRectangleRec(rect, RED);
-            DrawText(std::to_string(time).c_str(), 10, 10, 30, GREEN);
-            DrawText("=", 150, 10, 30, GREEN);
-            DrawText(std::to_string(timer.frame_count).c_str(), 200, 10, 30, RED);
-            DrawFPS(500, 10);
+            DrawRectangle(150, 50, 100, 100, ORANGE);
+
         EndDrawing();
     }
 
