06bce95a2bcf018452da326bbda66fde48474edf
glULTRA <7amakurd2002@gmail.com>
2022-06-24 17:47:06 +0300

    Circle Collisions

--- main.cpp
+++ main.cpp
@@ -11,6 +11,14 @@ int main(){
     // Initialize window.
     InitWindow(SCREEN_WIDHT, SCREEN_HEIGHT, SCREEN_TITLE);
 
+    // Circle Objects.
+    Vector2 circle_center = Vector2{300, 300};
+    Vector2 player_center = Vector2{ 50, 50};
+
+    bool isCollided = false;
+
+    float dx = 0.0f, dy = 0.0f, angle = 0.0f;
+    float dxx = 0.0f, dyy = 0.0f;
 
     // Target FPS.
     SetTargetFPS(60);
@@ -19,13 +27,34 @@ int main(){
     while (!WindowShouldClose())
     {
         // <----- UPDATE ----->
+        player_center = GetMousePosition();
+
+        isCollided = CheckCollisionCircles(player_center, 50, circle_center, 100);
+
+        if(isCollided){
+            // Find differences.
+            dx = player_center.x - circle_center.x;
+            dy = player_center.y - circle_center.y;
+
+            // Find angle.
+            angle = atan2f(dy, dx);
+
+            // Radius1 + Radius2
+            dxx = (50 + 100) * cosf(angle);
+            dyy = (50 + 100) * sinf(angle);
+
+            // Sum with player position.
+            player_center.x = circle_center.x + dxx;
+            player_center.y = circle_center.y + dyy;
+        }
 
         // <----- RENDER ----->
         BeginDrawing();
             // Clear Background
             ClearBackground(WHITE);
             // <--- DRAW --->
-            DrawRectangle(150, 50, 100, 100, ORANGE);
+            DrawCircleV(circle_center, 100, RED);
+            DrawCircleV(player_center, 50, BLUE);
 
         EndDrawing();
     }
