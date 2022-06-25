0fb043d4743d36e5654947d9ae8b747235dcfd82
glULTRA <7amakurd2002@gmail.com>
2022-06-25 14:55:43 +0300

    Tilemap Collision

--- main.cpp
+++ main.cpp
@@ -12,6 +12,9 @@ int main(){
     // Initialize window.
     InitWindow(SCREEN_WIDHT, SCREEN_HEIGHT, SCREEN_TITLE);
 
+    // Player Object.
+    Rectangle player = {200, 200, 50, 50};
+
     // Tilemap object.
     Tilemap tileblock(50, 2, 2);
 
@@ -22,6 +25,17 @@ int main(){
     while (!WindowShouldClose())
     {
         // <----- UPDATE ----->
+        if(IsKeyDown(KEY_W))
+            player.y -= 5.0f;
+        if(IsKeyDown(KEY_S))
+            player.y += 5.0f;
+        if(IsKeyDown(KEY_D))
+            player.x += 5.0f;
+        if(IsKeyDown(KEY_A))
+            player.x -= 5.0f;
+
+        // Check player Collision.
+        tileblock.CheckCollision(player);
 
         // <----- RENDER ----->
         BeginDrawing();
@@ -29,6 +43,7 @@ int main(){
             ClearBackground(WHITE);
             // <--- DRAW --->
             tileblock.Draw();
+            DrawRectangleRec(player, DARKBLUE);
 
         EndDrawing();
     }
