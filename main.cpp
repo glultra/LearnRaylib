770912391b175964c8178b37bce78d4f51a3ca2d
glULTRA <7amakurd2002@gmail.com>
2022-06-26 17:22:58 +0300

    Dark Moded

--- main.cpp
+++ main.cpp
@@ -34,7 +34,7 @@ int main(){
         // <----- RENDER ----->
         BeginDrawing();
             // Clear Background
-            ClearBackground(WHITE);
+            ClearBackground(Color{13,17,23,255});
             // <--- DRAW --->
             DrawRectangleRec(rect, RED);
             DrawText(std::to_string(time).c_str(), 10, 10, 30, GREEN);
