652582a3b3c78aa177792c6082e23eb00d0c44ed
glULTRA <7amakurd2002@gmail.com>
2022-06-27 17:25:25 +0300

    New Video Recorded

--- main.cpp
+++ main.cpp
@@ -1,6 +1,4 @@
 #include <raylib.h>
-#include <cmath>
-#include <time.h>
 #include <vector>
 
 #define SCREEN_WIDHT    600
@@ -8,47 +6,46 @@
 #define SCREEN_TITLE    "Raylib"
 
 int main(){
-    // Generate Random numbers.
-    srand(time(NULL));
-
     // Initialize window.
     InitWindow(SCREEN_WIDHT, SCREEN_HEIGHT, SCREEN_TITLE);
 
-    // Pixel vector.
+    // List of colors.
+    Color colors[] = {
+        GOLD,RED,GREEN,BLUE,PURPLE,
+        SKYBLUE,ORANGE,PINK,LIME,BEIGE
+    };
+
+    // Pixels.
     std::vector<Vector2> pixels;
 
-    Color colors[] = {
-        RED,GREEN,BLUE,PURPLE,ORANGE,YELLOW,BLACK,BROWN
-    };
+    // Set Each pixel's position.
+    for (int i = 0; i < 50000; i++)
+    {
+        Vector2 pixel;
+        pixel.x = GetRandomValue(0, SCREEN_WIDHT);
+        pixel.y = GetRandomValue(0, SCREEN_HEIGHT);
+        pixels.push_back(pixel);
+    }
+    
 
     // Render loop.
     while (!WindowShouldClose())
     {
         // <----- UPDATE ----->
 
-        // Generating random pixels
-        // maybe 600 is too much pixels u can make it 300 or less.
-        for(int i = 0; i < 300; i++){
-            for(int j = 0; j < 300; j++){
-                Vector2 pix;
-                pix.x = rand() % 600;
-                pix.y = rand() % 600;
-                pixels.push_back(pix);
-            }
-        }
-
         // <----- RENDER ----->
         BeginDrawing();
             // Clear Background
-            ClearBackground(WHITE);
+            ClearBackground(Color{13,17,23,255});
+            // ClearBackground(WHITE);
             // <--- DRAW --->
-            for(Vector2 pixel : pixels){
-                DrawPixelV(pixel, colors[rand() % 8]);
+            for (auto &pixel : pixels)
+            {
+                DrawPixel(pixel.x, pixel.y, colors[GetRandomValue(0, 9)]);
             }
-        EndDrawing();
+            
 
-        // Clear vector.
-        pixels.clear();
+        EndDrawing();
     }
     
 }
