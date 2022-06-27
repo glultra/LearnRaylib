cadf5e346166089b3aba83a594a4f1efd0219caf
glULTRA <7amakurd2002@gmail.com>
2022-06-27 16:30:43 +0300

    New Video Recorded

--- main.cpp
+++ main.cpp
@@ -18,12 +18,12 @@ int main(){
             // Clear Background
             ClearBackground(Color{13,17,23,255});
             // <--- DRAW --->
-            // DrawLine(200, 200, 300, 300, RED);
-            // DrawLineEx(Vector2{200, 200}, Vector2{300, 200}, 6, RED);
-            // DrawLineBezier(Vector2{200, 200}, Vector2{300, 300}, 3, ORANGE);
-            // DrawLineBezier(Vector2{300, 300}, Vector2{400, 200}, 3, GREEN);
-            // DrawLineBezierCubic(Vector2{200, 200}, Vector2{400,200}, Vector2{250,300}, Vector2{350,100}, 3, BLUE);
-            DrawLineBezierQuad(Vector2{200, 200}, Vector2{400, 200}, GetMousePosition(), 3, BLUE);
+            // DrawLine(200, 200, 300, 300, BLUE);
+            // DrawLineEx(Vector2{200, 200}, Vector2{300, 200}, 7,YELLOW);
+            // DrawLineBezier(Vector2{200, 200}, Vector2{300, 300}, 7, ORANGE);
+            // DrawLineBezier(Vector2{300, 300}, Vector2{400, 200}, 7, BLUE);
+            // DrawLineBezierQuad(Vector2{200, 200}, Vector2{400, 200}, GetMousePosition(), 3, ORANGE);
+            DrawLineBezierCubic(Vector2{200, 200}, Vector2{400, 200}, Vector2{250, 300}, Vector2{350, 100}, 3, PURPLE);
         EndDrawing();
     }
     
