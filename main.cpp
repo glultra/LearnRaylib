737a200df120561f55afb903ee9cf628ce5f3475
glULTRA <7amakurd2002@gmail.com>
2022-06-27 16:12:04 +0300

    New Video Recorded

--- main.cpp
+++ main.cpp
@@ -8,8 +8,11 @@ int main(){
     // Initialize window.
     InitWindow(SCREEN_WIDHT, SCREEN_HEIGHT, SCREEN_TITLE);
 
-    // Circle properties
-    Vector2 circle_center = Vector2{300, 300};
+    // Rectangle object.
+    Rectangle rec = Rectangle{100, 80, 200, 80};
+
+    // Circle attribute.
+    Vector2 center_pos = Vector2{300, 300};
 
     // Render loop.
     while (!WindowShouldClose())
@@ -21,12 +24,15 @@ int main(){
             // Clear Background
             ClearBackground(Color{13,17,23,255});
             // <--- DRAW --->
-            // DrawCircle(300, 300, 100, RED);
-            // DrawCircleV(circle_center, 100, ORANGE);
-            // DrawCircleGradient(circle_center.x, circle_center.y, 100, ORANGE, PINK);
-            // DrawCircleLines(circle_center.x, circle_center.y , 100, BLACK);
-            DrawCircleSector(circle_center, 100, 0, 270, 100, BLUE);
-            DrawCircleSectorLines(circle_center, 100, 0, 270, 100, YELLOW);
+            // DrawCircleV(center_pos, 50, GOLD);
+            // DrawCircleLines(center_pos.x, center_pos.y, 50, BLUE);
+            // DrawCircleGradient(center_pos.x, center_pos.y, 100, SKYBLUE, BLACK);
+            DrawCircleSector(center_pos, 100, 0, 270, 100, MAROON);
+            DrawCircleSectorLines(center_pos, 100, 0, 270, 100, GOLD);
+            // DrawEllipse(center_pos.x, center_pos.y, 100, 50, Color{100, 120, 255, 255});
+            // DrawEllipseLines(center_pos.x, center_pos.y, 100, 50, Color{100, 120, 255, 255});
+            DrawRectangleRounded(rec, 0.9f, 100, GOLD);
+            DrawRectangleRoundedLines(rec, 0.9f, 100, 7,  BLUE);
 
         EndDrawing();
     }
