fe2f054ee5fa0a565300a5da1d72130bfafee5ba
glULTRA <7amakurd2002@gmail.com>
2022-06-27 14:10:19 +0300

    Recorded new video

--- main.cpp
+++ main.cpp
@@ -8,12 +8,13 @@ int main(){
     // Initialize window.
     InitWindow(SCREEN_WIDHT, SCREEN_HEIGHT, SCREEN_TITLE);
 
-    // Rectangle attributes
-    Vector2 rec_pos = Vector2{300, 150};
-    Vector2 rec_size = Vector2{100, 100};
-    Vector2 rec_origin = Vector2{50, 50};
+    // Rectangle Properties.
+    Vector2 rec_pos = Vector2{100, 80};
+    Vector2 rec_size = Vector2{200, 100};
 
-    Rectangle rect = Rectangle{rec_pos.x, rec_pos.y, rec_size.x ,rec_size.y};
+    // Rectangle object.
+    Rectangle rec = Rectangle{400, 100, 100, 200};
+    
 
     // Render loop.
     while (!WindowShouldClose())
@@ -23,20 +24,25 @@ int main(){
         // <----- RENDER ----->
         BeginDrawing();
             // Clear Background
-            ClearBackground(Color{13,17,23,255});
+            ClearBackground(Color{23, 21, 21});
             // <--- DRAW --->
-            rect.y = 100;
-            DrawRectangleGradientEx(rect, ORANGE, PURPLE, GREEN, PINK);
-            DrawRectangleLines(rect.x, rect.y, rect.width, rect.height, BLACK);
-            DrawRectangleLinesEx(rect, 3, BLACK);
-            rect.y = 350;
-            DrawRectanglePro(rect, rec_origin, 45, ORANGE);
-
-            rect.y = 470;
-            DrawRectangleRounded(rect, 100, 100, RED);
-            DrawRectangleRoundedLines(rect, 100, 100, 7, BLUE);
-
+            DrawRectangle(250, 250, 100, 100, ORANGE);
+            DrawRectangleV(rec_pos, rec_size, BLUE);
+            rec.x = 200;
+            DrawRectangleRec(rec, GREEN);
+            // DrawRectangleLines(rec.x, rec.y, rec.width, rec.height, PURPLE);
+            DrawRectangleLinesEx(rec, 15, SKYBLUE);
+            rec.x = 120;
+            DrawRectanglePro(rec, Vector2{rec.width / 2, rec.height / 2}, 45, GOLD);
+            rec.x = 450;
+            rec.y = 50;
+            DrawRectangleGradientV(rec.x, rec.y, rec.width, rec.height, RED, SKYBLUE);
+            // DrawRectangleGradientH(rec.x, rec.y, rec.width, rec.height, RED, SKYBLUE);
+            rec.y = 350;
+            DrawRectangleGradientEx(rec, PINK, SKYBLUE, RED, GOLD);
         EndDrawing();
     }
-
-} 
+    
+    // De-Initialize OpenGL Context.
+    CloseWindow();
+}
