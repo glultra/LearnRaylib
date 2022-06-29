4784152ebe8837d7a4dd78256f55b2bdec89e5f9
glULTRA <7amakurd2002@gmail.com>
2022-06-29 17:06:38 +0300

    Window Options

--- main.cpp
+++ main.cpp
@@ -10,9 +10,22 @@
 int main(){
     // Set Anti Aliasing Flags.
     SetConfigFlags(FLAG_MSAA_4X_HINT);
+    SetConfigFlags(FLAG_INTERLACED_HINT);
+    SetConfigFlags(FLAG_VSYNC_HINT);
 
     // Initialize window.
     InitWindow(SCREEN_WIDHT, SCREEN_HEIGHT, SCREEN_TITLE);
+
+    // Image object.
+    Image icon_img = LoadImage("./res/icons/koo.png"); 
+
+    // Window Options
+    // SetWindowSize(300, 300);
+    SetWindowIcon(icon_img);
+    SetWindowState(FLAG_WINDOW_RESIZABLE);
+    SetWindowState(FLAG_WINDOW_MAXIMIZED);
+    SetWindowState(FLAG_FULLSCREEN_MODE);
+    
 
     // Camera 2D.
     Camera2D camera{0};
@@ -28,6 +41,7 @@ int main(){
     while (!WindowShouldClose())
     {
         // <----- UPDATE ----->
+        SetWindowTitle(std::to_string(GetFPS()).append(" FPS").c_str());
 
         // Update Camera
         camera.zoom += GetMouseWheelMove() * 0.05f;
@@ -38,7 +52,7 @@ int main(){
             ClearBackground(Color{13,17,23,255});
             // <--- DRAW --->
             BeginMode2D(camera);
-                DrawPoly(Vector2{300, 300}, 100, 50, 0, BLUE);
+                DrawPoly(Vector2{GetScreenWidth() / 2.0f, GetScreenHeight() / 2.0f}, 100, 50, 0, BLUE);
                 DrawLineBezier(Vector2{0,0}, GetMousePosition(), 7, RED);
             EndMode2D();
         EndDrawing();
