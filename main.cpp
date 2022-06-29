ae316e63acac77bb8904963ffebf9ae68b7ebbeb
glULTRA <7amakurd2002@gmail.com>
2022-06-29 17:43:14 +0300

    Drag/Drop Example

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 #include <iostream>
 #include <cmath>
 #include <string.h>
-#include "src/tilemap.h"
 
 #define SCREEN_WIDHT    600
 #define SCREEN_HEIGHT   600
@@ -12,6 +11,12 @@ int main(){
     // Initialize window.
     InitWindow(SCREEN_WIDHT, SCREEN_HEIGHT, SCREEN_TITLE);
 
+    // Window Option.
+    SetWindowState(FLAG_WINDOW_RESIZABLE);
+
+    // Texture 2D. 
+    Texture2D texture = LoadTexture("./res/images/light.png");
+
 
     // Target FPS.
     SetTargetFPS(60);
@@ -20,15 +25,34 @@ int main(){
     while (!WindowShouldClose())
     {
         // <----- UPDATE ----->
+        // Case 1: Responsive Statcily depends to the image size.
+        // SetWindowSize(texture.width, texture.height);
+        // Case 2: Responsive By Window Size.
+        texture.width = GetScreenWidth();
+        texture.height = GetScreenHeight();
+
+        // Check if Draggged/Dropped.
+        if(IsFileDropped()){
+            UnloadTexture(texture);
+            FilePathList source = LoadDroppedFiles();
+            if(source.count == 1){
+                if(IsFileExtension(source.paths[0], ".png"))
+                    texture = LoadTexture(source.paths[0]);
+            }
+            UnloadDroppedFiles(source);
+        }
 
         // <----- RENDER ----->
         BeginDrawing();
             // Clear Background
             ClearBackground(Color{13,17,23,255});
             // <--- DRAW --->
-
+            DrawTexture(texture, 0, 0, WHITE);
         EndDrawing();
     }
+
+    // Unload texture.
+    UnloadTexture(texture);
 
     // De Initialization of OpenGL Context.
     CloseWindow();
