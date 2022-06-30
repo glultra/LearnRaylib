4a4aedc6eda8fb905a9298d3ec780e54c768a968
glULTRA <7amakurd2002@gmail.com>
2022-06-30 22:12:37 +0300

    Cursor Flag and new hover

--- main.cpp
+++ main.cpp
@@ -8,6 +8,9 @@
 #define SCREEN_HEIGHT   600
 #define SCREEN_TITLE    "Raylib"
 
+ bool isButtonHover;
+ bool isSliderHover;
+
 // Gui function protypes.
 void GuiSliderMusic(Rectangle rec, Music& music, Color color);
 void GuiPlayButton(Vector2 position, Music& music, float radius, Color bgColor, Color fgColor);
@@ -26,10 +29,11 @@ int main(){
     Music music = LoadMusicStream("./res/music/quraan/101.mp3");
     // music.looping = false;
 
+    SeekMusicStream(music, 10);
     PlayMusicStream(music);
 
     // Slider rec.
-    Rectangle seek_rec{50, 400, 300, 10};
+    Rectangle seek_rec{50, 400, 300, 20};
     seek_rec.x = GetScreenWidth() / 2.0f - seek_rec.width/2.0f;
 
     // Texture2D.
@@ -47,6 +51,12 @@ int main(){
         // <----- UPDATE ----->
         float tex_centerx = GetScreenWidth()/2.0f  - texture.width/2.0f;
         float tex_centery = GetScreenWidth()/2.0f  - texture.height/2.0f;
+
+        if(isButtonHover || isSliderHover){
+            SetMouseCursor(MOUSE_CURSOR_POINTING_HAND);
+        }else{
+            SetMouseCursor(MOUSE_CURSOR_DEFAULT);
+        }
         
         // Update Music Buffer.
         UpdateMusicStream(music);
@@ -57,8 +67,9 @@ int main(){
             ClearBackground(Color{13,17,23,255});
             // <--- DRAW --->
             DrawTexture(texture, tex_centerx, tex_centery - 100, WHITE);
+            GuiPlayButton(Vector2{GetScreenWidth()/2.0f, GetScreenHeight()/2.0f + 200}, music, 50, SKYBLUE, WHITE);
             GuiSliderMusic(seek_rec, music, SKYBLUE);
-            GuiPlayButton(Vector2{GetScreenWidth()/2.0f, GetScreenHeight()/2.0f + 200}, music, 50, DARKGRAY, WHITE);
+            DrawText("Al-Qari'ah", GetScreenWidth()/2.0f - MeasureText("Al-Qari'ah", 30)/2.0f, GetScreenHeight()/2.0f + 50, 30, WHITE);
         EndDrawing();
     }
 
@@ -73,7 +84,7 @@ void GuiSliderMusic(Rectangle rec, Music& music, Color color){
 
     float time_played   = GetMusicTimePlayed(music);
     float max_second    = GetMusicTimeLength(music);
-    bool isSliderHover  = CheckCollisionPointRec(GetMousePosition(), rec);
+    isSliderHover  = CheckCollisionPointRec(GetMousePosition(), rec);
 
     if(isSliderHover && IsMouseButtonDown(MOUSE_BUTTON_LEFT)){
         float dx = GetMousePosition().x - rec.x;
@@ -96,27 +107,28 @@ void GuiSliderMusic(Rectangle rec, Music& music, Color color){
     ss << (int)time_played % 60;
 
     // Render
-    DrawRectangleRounded(slider, 1.0f, 7, color); // Slider
+    DrawRectangleRounded(slider, 1.0f, 7, isSliderHover ? color : Fade(color, 0.9f)); // Slider
     DrawRectangleRounded(rec, 1.0f, 7, Fade(color, 0.3f)); // Rec
     DrawRectangleRoundedLines(rec, 3, 1.0f, 7, Fade(color, 0.7f)); // Border
     DrawCircleV(circle_center, point_radius, BLUE);
     DrawCircleLines(circle_center.x, circle_center.y, point_radius, BLACK);
-    DrawText(ss.str().c_str(), rec.x - MeasureText(ss.str().c_str() , 23) - 23, rec.y, 23, GREEN);
+    DrawText(ss.str().c_str(), rec.x - MeasureText(ss.str().c_str() , 23) - 23, rec.y, 23, WHITE);
     ss.clear();
     ss = std::stringstream("");
     ss << (int)max_second / 60 << ":"; // first show mins
     ss << (int)max_second % 60;
-    DrawText(ss.str().c_str(), rec.x + rec.width + MeasureText(ss.str().c_str() , 23) - 20, rec.y, 23, GREEN);
+    DrawText(ss.str().c_str(), rec.x + rec.width + MeasureText(ss.str().c_str() , 23) - 18, rec.y, 23, WHITE);
 }
 
 void GuiPlayButton(Vector2 position, Music& music, float radius, Color bgColor, Color fgColor){
     // Update 
     static bool isPlayed = true;
-    bool isButtonHover = CheckCollisionPointCircle(position, GetMousePosition(), radius);
+    isButtonHover = CheckCollisionPointCircle(position, GetMousePosition(), radius);
     float point = radius / 3.0f; 
 
-    if(isButtonHover && IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+    if(isButtonHover && IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
         isPlayed = isPlayed ? false : true;
+    }
 
     // Render
     DrawCircleV(position, radius, isButtonHover ? bgColor: Fade(bgColor, 0.7f));
