10e5e29c9cf15f0b49024b66275d38bbde371516
glULTRA <7amakurd2002@gmail.com>
2022-07-03 15:21:51 +0300

    Collision Bug fixed

--- src/tilemap.cpp
+++ src/tilemap.cpp
@@ -21,15 +21,15 @@ void Tilemap::CheckCollision(Rectangle& player){
                 player.x = block.x - player.width;
             }
             // Checking for bottom side of the player and top side of the wall.
-            if(player.y < block.y && player.y + player.height > block.y && collidedRec.width > 10){
+            else if(player.y < block.y && player.y + player.height > block.y && collidedRec.width > 10){
                 player.y = block.y - player.height;
             }
             // Checking for left side of the player and right side of the wall.
-            if(player.x > block.x && player.x < block.x + block.width && collidedRec.height > 10){
+            else if(player.x > block.x && player.x < block.x + block.width && collidedRec.height > 10){
                 player.x = block.x + player.width;
             }
             // Checking for top side of the player and bottom side of the wall.
-            if(player.y > block.y && player.y < block.y + block.height && collidedRec.width > 10){
+            else if(player.y > block.y && player.y < block.y + block.height && collidedRec.width > 10){
                 player.y = block.y + player.height;
             }
         }
