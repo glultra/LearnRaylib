d730f9bc14f1b6d67b2782941ae3a138bb296085
glULTRA <7amakurd2002@gmail.com>
2022-07-03 13:12:20 +0300

    New Video Recorded

--- main.cpp
+++ main.cpp
@@ -1,9 +1,7 @@
 #include <raylib.h>
 #include <string.h>
 #include <iostream>
-#include <cmath>
 #include "src/timer.h"
-#include <iomanip>
 
 #define SCREEN_WIDHT    600
 #define SCREEN_HEIGHT   600
@@ -14,13 +12,13 @@ int main(){
     InitWindow(SCREEN_WIDHT, SCREEN_HEIGHT, SCREEN_TITLE);
 
     // Rectangle object.
-    Rectangle rect = Rectangle{250, 250, 100, 100};
+    Rectangle rec{250, 250, 100, 100};
 
     // Timer object.
-    Timer timer;
-    
+    Timer timer = Timer{};
+
     // Settarget fps.
-    SetTargetFPS(60); // 60 picture per second -> 1s = 60frame
+    SetTargetFPS(60); 
 
     // Render loop.
     while (!WindowShouldClose())
@@ -28,19 +26,18 @@ int main(){
         // <----- UPDATE ----->
         float time = GetTime();
         timer.UpdateTime(time);
-
-        rect.x = 200 + (timer.xValue * 150 );
-        rect.y = 200 + (timer.yValue * 150 );
         
+        rec.x = 250 + (timer.xValue * 100); 
+        rec.y = 250 + (timer.yValue * 100); 
+       
         // <----- RENDER ----->
         BeginDrawing();
             // Clear Background
             ClearBackground(Color{13,17,23,255});
             // <--- DRAW --->
-            DrawRectangleRec(rect, BLUE);
-            time = round(time * 100) / 100;
-            DrawText(std::to_string(time).substr(0,3).append(" s =  ").c_str(), 10, 10, 30, GREEN);
-            DrawText(std::to_string(timer.frame_count).append(" FPS").c_str(), 120, 10, 30, PURPLE);
+            DrawRectangleRec(rec, BLUE);
+            DrawText(std::to_string((int)time).c_str(), 10, 10, 30, BLUE);
+            DrawText(std::to_string((int)timer.frame_count).c_str(), 100, 10, 30, BLUE);
             DrawFPS(500, 10);
         EndDrawing();
     }
