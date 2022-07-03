dc4652aa30eac323bff8c1f81a4120d282b782a4
glULTRA <7amakurd2002@gmail.com>
2022-07-03 22:46:16 +0300

    Restored src from 23-Tilemap-Collision

--- src/tilemap.cpp
+++ src/tilemap.cpp
@@ -1,18 +1,34 @@
 #include "tilemap.h"
 
+void Tilemap::InitTilemap(){
+    // Initialize each block's position depends to the tilemap.
+    for (int i = 0; i < SIZE_Y; i++)
+    {
+        for(int j = 0; j < SIZE_X; j++){
+            if(tilemap[i][j] == ' ') continue; // Skip.
+            if(tilemap[i][j] == '#'){
+                Rectangle block;
+                block.width = 50;
+                block.height = 50;
+                block.x = position.x + (block.width  + offset.x) * j; // Add each column's block.
+                block.y = position.y + (block.height + offset.y) * i; // Add each row's block.
+                blocks.push_back(block);
+            }
+        }
+    }
+    
+}
 void Tilemap::Draw(Color color){
     for (auto block : blocks)
     {
-        DrawRectangleRec(block, color);   
+        DrawRectangleRec(block, color);
     }
-        
+    
 }
-
-void Tilemap::CheckCollision(Rectangle& player){
+void Tilemap::CheckCollision(Rectangle &player){
     for (auto block : blocks)
     {
-        bool isCollided = CheckCollisionRecs(player, block);
-        // Collided Rectangle.
+        bool isCollided = CheckCollisionRecs(block, player);
         Rectangle collidedRec = GetCollisionRec(player, block);
 
         if(isCollided){
@@ -21,29 +37,24 @@ void Tilemap::CheckCollision(Rectangle& player){
                 player.x = block.x - player.width;
             }
             // Checking for bottom side of the player and top side of the wall.
-            if(player.y < block.y && player.y + player.height > block.y && collidedRec.width > 10){
+            else if(player.y < block.y && player.y + player.height > block.y && collidedRec.width > 10){
                 player.y = block.y - player.height;
             }
             // Checking for left side of the player and right side of the wall.
-            if(player.x > block.x && player.x < block.x + block.width && collidedRec.height > 10){
+            else if(player.x > block.x && player.x < block.x + block.width && collidedRec.height > 10){
                 player.x = block.x + player.width;
             }
             // Checking for top side of the player and bottom side of the wall.
-            if(player.y > block.y && player.y < block.y + block.height && collidedRec.width > 10){
+            else if(player.y > block.y && player.y < block.y + block.height && collidedRec.width > 10){
                 player.y = block.y + player.height;
             }
         }
-
     }
     
 }
 void Tilemap::SetPosition(Vector2 position){
     this->position = position;
-    blocks.clear();
-    InitTilemap();
 }
 void Tilemap::SetOffset(Vector2 offset){
     this->offset = offset;
-    blocks.clear();
-    InitTilemap();
 }
