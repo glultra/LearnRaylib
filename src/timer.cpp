0db4a7d7ceb4adc866d8ecea1b9e7a3ff9ac73f1
glULTRA <7amakurd2002@gmail.com>
2022-07-04 15:03:20 +0300

    Restored src from 24-Camera2D

--- src/timer.cpp
+++ src/timer.cpp
@@ -1,14 +1,9 @@
 #include "timer.h"
-#include <cmath>
 
 void Timer::UpdateTime(float time){
-    xValue = std::cos(time); // from +1.0 to -1.0
-    yValue = std::sin(time); // from -1.0 to +1.0
-    xValuePos = (xValue / 2.0) + 0.5; // from 1.0 to 0.0
-    yValuePos = (yValue / 2.0) + 0.5; // from 0.0 to 1.0
-
+    xValue = std::cos(time * speed);
+    yValue = std::sin(time * speed);
     deltaTime = time - lastFrame;
     lastFrame = time;
-    
     frame_count++;
 }
