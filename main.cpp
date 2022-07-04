7283a19ee9248c4597738f137eda247b995eb8cb
glULTRA <7amakurd2002@gmail.com>
2022-07-04 11:40:51 +0300

    New Video Recorded

--- main.cpp
+++ main.cpp
@@ -12,30 +12,37 @@
 
 using namespace std;
 
-int main(){
-    // Initialize window.
-    InitWindow(SCREEN_WIDHT, SCREEN_HEIGHT, SCREEN_TITLE);
-
-    // Tile blocks.
+vector<Rectangle> MakeTileblocks(float offset)
+{
     vector<Rectangle> blocks;
-
-    // Set each block's Position.
-    for (int  i = 0; i < SCREEN_WIDHT / BLOCK_SIZE; i++)
+    for (int i = 0; i < SCREEN_WIDHT / BLOCK_SIZE; i++)
     {
         for (int j = 0; j < SCREEN_HEIGHT / BLOCK_SIZE; j++)
         {
             Rectangle block;
             block.width = BLOCK_SIZE; block.height = BLOCK_SIZE;
-            block.x  = 0.0f + (BLOCK_SIZE) * j;
-            block.y  = 0.0f + (BLOCK_SIZE) * i;
+            block.x = 0.0f + (BLOCK_SIZE + offset) * i;
+            block.y = 0.0f + (BLOCK_SIZE + offset) * j;
             blocks.push_back(block);
-        }
-        
+        }   
     }
+    return blocks;
+}
 
-    // Tile Color state.
-    Color tile_color = RED;
+int main(){
+    // Enable MSAA 4X.
+    SetConfigFlags(FLAG_MSAA_4X_HINT);
     
+    // Initialize window.
+    InitWindow(SCREEN_WIDHT, SCREEN_HEIGHT, SCREEN_TITLE);
+
+    // Tile blocks.
+    vector<Rectangle> blocks;
+    blocks = MakeTileblocks(1.0f);
+
+    // Tile color state.
+    Color tile_color = RED;
+
 
     // Target FPS.
     SetTargetFPS(60);
@@ -44,16 +51,21 @@ int main(){
     while (!WindowShouldClose())
     {
         // <----- UPDATE ----->
-        float time = GetTime();
         static int frame_count = 0;
-        frame_count++; // 1s = 60f
-
-        // When ever it passes each two second.
+        frame_count++;
         if(frame_count % 120 == 0){
             tile_color.r = GetRandomValue(0, 255);
             tile_color.g = GetRandomValue(0, 255);
             tile_color.b = GetRandomValue(0, 255);
             tile_color.a = 255;
+        }
+
+        static float offset = 0.0f;
+
+        if(IsKeyDown(KEY_D)){
+            blocks = MakeTileblocks(offset+=0.1f);
+        }else if (IsKeyDown(KEY_A)){
+            blocks = MakeTileblocks(offset-=0.1f);
         }
 
         // <----- RENDER ----->
@@ -64,9 +76,8 @@ int main(){
             float fade_rate = 1.0f;
             for (int i = 0; i < blocks.size(); i++)
             {
-                fade_rate = i % (SCREEN_WIDHT/BLOCK_SIZE) == 0 ? fade_rate-=0.07f : fade_rate;
+                fade_rate = i % (SCREEN_WIDHT / BLOCK_SIZE) == 0 ? fade_rate-=0.07f : fade_rate;
                 DrawRectangleRec(blocks[i], Fade(tile_color, fade_rate));
-               
             }
             
 
