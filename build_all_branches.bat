92d87edaa7dd0e7e325764cde2848604ca18616c
glULTRA <7amakurd2002@gmail.com>
2022-07-05 18:15:56 +0300

    Fixed!!

--- build_all_branches.bat
+++ build_all_branches.bat
@@ -10,6 +10,6 @@ for /F "tokens=*" %%A in  (branches.txt) do  (
    Call move main.exe .\examples\%%A.exe
 )
 
-echo "d" | xcopy .\res\ .\examples\res /E -D
+CALL echo "d" | xcopy .\res\ .\examples\res /E -D
 
 exit
