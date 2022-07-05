7dc0e8b00a0c045e214dea616ef85abd16337fb6
glULTRA <7amakurd2002@gmail.com>
2022-07-05 14:35:51 +0300

    Added Example builder

---
+++ build_all_branches.bat
@@ -0,0 +1,15 @@
+@echo off
+
+if not exist "examples\res" (
+    mkdir "examples\res"
+    xcopy .\res\ .\examples\res /E
+)
+for /F "tokens=*" %%A in  (branches.txt) do  (
+   CALL git checkout %%A
+   CALL make compile
+   Call move main.exe .\examples\%%A.exe
+)
+
+CALL git checkout master
+
+exit
