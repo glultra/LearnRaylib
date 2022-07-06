f4b4d70217ecd80d5dbee7fcc8b67e9367af950c
glULTRA <7amakurd2002@gmail.com>
2022-07-06 21:29:23 +0300

    5-3.gif Added

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 > Output:
 > <p align="center">
->   <img src="https://github.com/glULTRA/LearnRaylib/blob/z-Course-Resources/course_res/images/1-2.png">
+>   <img src="https://github.com/glULTRA/LearnRaylib/blob/z-Course-Resources/course_res/images/5-3.gif">
 > </p>
 > <br>
 > <a href="https://github.com/glULTRA" class="btn btn-primary"> &copy;Mohammed Khalid </a>
--- main.cpp
+++ main.cpp
@@ -10,19 +10,28 @@ int main(){
     InitWindow(SCREEN_WIDHT, SCREEN_HEIGHT, SCREEN_TITLE);
 
     // Rectangle object.
-    Rectangle rec = Rectangle{200 , 100, 200, 400};
+    Rectangle rec{225, 150, 150, 300};
 
     // Render loop.
     while (!WindowShouldClose())
     {
         // <----- UPDATE ----->
+        float time = GetTime();
+        float xValuePos = std::sin(time) / 2.0f + 0.5f;
+        float yValuePos = std::cos(time) / 2.0f + 0.5f;
 
         // <----- RENDER ----->
         BeginDrawing();
             // Clear Background
             ClearBackground(Color{23, 21, 21});
             // <--- DRAW --->
-            DrawRectangleGradientEx(rec, Fade(PINK, std::sin(GetTime())/2.0f + 0.5f), Fade(SKYBLUE, std::sin(GetTime())/2.0f + 0.5f), Fade(RED, std::cos(GetTime())/2.0f + 0.5f), Fade(GOLD, std::cos(GetTime())/2.0f + 0.5f));
+           DrawRectangleGradientEx(
+                rec,
+                Fade(RED, xValuePos),
+                Fade(PINK, yValuePos),
+                Fade(SKYBLUE, yValuePos),
+                Fade(ORANGE, xValuePos)
+           );
         EndDrawing();
     }
     
